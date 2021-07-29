b1d889f7ad733637893ef85f49e32a7d9e30df9f
Vectadeus <Mirochavchanidze4@gmail.com>
2021-07-29 17:58:35 +0400

    HitmanMovement

--- 3D Game/Assets/Scripts/EnemyMovement.cs
+++ 3D Game/Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -25,27 +25,46 @@ public class EnemyMovement : MonoBehaviour
     //ToFindNearest
     public Collider Nearest;
 
+
+    // Animations
+    Animator anim;
+    bool IsMoving;
+
+
+
+
     private void OnEnable()
     {
         Agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FindPlayerQoroutine());
+        StartCoroutine(CheckMovement());
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        Agent.SetDestination(player.transform.position);
-        FindPlayer();
+
+        PlayAnimations();
+        EnemyActions();
+
     }
-    
 
 
 
 
+
+    IEnumerator FindPlayerQoroutine()
+    {
+        FindPlayer();
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(FindPlayerQoroutine());
+    }
 
 
 
@@ -54,28 +73,95 @@ public class EnemyMovement : MonoBehaviour
     void FindPlayer()
     {
         colliders = Physics.OverlapSphere(transform.position, PlayersFindRadius, PlayerMask);
-
         foreach(Collider collider in colliders)
         {
-            if(Nearest = null)
-            {
-            Nearest = collider;
-                Debug.Log("HIU");
-            }
-            if(Nearest != null && Vector3.Distance(transform.position , collider.transform.position) < Vector3.Distance(transform.position, Nearest.transform.position))
+
+            if (Nearest == null)
             {
                 Nearest = collider;
-                Debug.Log(collider.name);
+            }
+            if (Nearest != null && Vector3.Distance(transform.position, collider.transform.position) < Vector3.Distance(transform.position, Nearest.transform.position))
+            {
+                Nearest = collider;
             }
 
-            
+        }
+
+    }
+
+   IEnumerator CheckMovement()
+    {
+        Vector3 prevpos = transform.position;
+        yield return new WaitForSeconds(0.05f);
+        if(prevpos.x == transform.position.x && prevpos.z == transform.position.z)
+        {
+            IsMoving = false;
+        }
+        else
+        {
+            IsMoving = true;
+        }
+        StartCoroutine(CheckMovement());
+    }
+    
+
+    void PlayAnimations()
+    {
+        if (IsMoving)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
         }
     }
+    void EnemyActionss()
+    {
+        if(Vector3.Distance(transform.position , Nearest.transform.position) <= SightRange)
+        {
+            //Gaekite
+        }
+        if(Vector3.Distance(transform.position , Nearest.transform.position) <= AttackRange)
+        {
+            //ATTACK
+        }
+        Agent.SetDestination(Nearest.transform.position);
+    }
+
+
+
+
+
+
 
     void EnemyActions()
     {
-        
+        if(Nearest != null)
+        {
+        if(Vector3.Distance(transform.position , Nearest.transform.position) <= SightRange)
+        {
+            Agent.SetDestination(Nearest.transform.position);
+        }
+
+        if(Vector3.Distance(transform.position , Nearest.transform.position) <= AttackRange)
+        {
+            Agent.SetDestination(transform.position);
+        }
+
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
 
     private void OnDrawGizmos()
     {
